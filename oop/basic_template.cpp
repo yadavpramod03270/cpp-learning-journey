d9b36f7a8bec916308a6c8799b90467c7ce6b47d
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2025-12-26 10:48:13 +0530

    Update and rename README.md to basic_template.cpp

---
+++ oop/basic_template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class sample{
+public:
+void print_data(){
+cout<<"this is the basic blue print of class"<<endl;
+}
+};
+int main(){
+  sample s;
+  s.print_data();
+  return 0;
+}
